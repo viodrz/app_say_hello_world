43c03c504c8b4c577bee62cd94dc414e77222c1d
Violetta Surya Pratama <viodrz@gmail.com>
2022-03-29 15:51:09 +0700

    contoh penerapan app module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/viodrz/app_say_hello_world.git
+
+go 1.17
+
+require github.com/viodrz/go_hello_world/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/viodrz/go_hello_world/v2"
+)
+
+func main() {
+	fmt.Println(go_hello_world.SayHello("vio"))
+}
